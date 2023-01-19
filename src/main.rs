8234cebcb9a0452585620ab678d853b90b23077d
Armandase <adamiens@student.42angouleme.fr>
2023-01-19 15:00:24 +0100

    miss compilation

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,38 @@
-use std::fs;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 fn main(){
     let mut path = String::new();
+    let mut counter = 0;
+    let mut number: u64;
 
     path.push_str("/proc/stat");
-    let file = fs::read_to_string(path)
-        .expect("Permission denied");
-    for i in 0..16{
-    let mut counter = 0;
-    loop{
-
-    }
-        for byte in file.split_whitespace() {
-            println!("Content : {}", byte);
-            let value: f64 = byte.trim().parse().expect("Parse error");
-            for n in 1..100{
-                if n > value as i32 {
-                    print!("\x1b[96m|\x1b[0m");
-                } else {
-                    print!("\x1b[92m|\x1b[0m");
-                }
-            }
-            break ;
+    while let Ok(line) = read_lines(path){
+        let mut index = 1;
+        let mut user = 0;
+        if counter == 0{
+            counter += 1;
+            continue ;
         }
-        println!("\n");
+        number = 0;
+        let byte = line.split_whitespace();
+        print!("{}", byte);
+        for byte in line.split_whitespace() {
+            let value = byte.trim().parse.expect("Parsing error");
+            number += value;
+            if index == 4{
+                user = value;
+            }
+            index += 1;
+        }
+        let load = 100 - (user / number) * 100;
+        println!("{}%", load);
     }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
